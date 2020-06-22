269b15abd0be21a41280d365a628a1ca2642c90f
Wil <35899839+deltaex1@users.noreply.github.com>
2020-06-21 21:16:52 -0400

    Update RandomXP.py
    updated 'path' to use current working directory
    updated to use secrets module instead of random module for a cryptographically secured randomization instead of pseudo-random generators (a pattern was noticed in the selection of files)

--- RandomXP.py
+++ RandomXP.py
@@ -5,17 +5,17 @@ Created on Thu Jun 18 20:15:54 2020
 @author: Wilson
 """
 
-import os, random
+import os, secrets
 
-path = "X:\\"
+path = "./"
 #extension = ('.mp4','.wmv','.mkv','.avi','.flv')
 extension = ('jpg','jpeg','png')
 
 count = 0
 while count < 5:
-    file = (random.choice(os.listdir(path)))
+    file = (secrets.choice(os.listdir(path)))
     while file.lower().endswith(extension) is False:
-        file = (random.choice(os.listdir(path)))
+        file = (secrets.choice(os.listdir(path)))
         print('File selected has an incorect extension, retrying...')   
 
     print('Opening file: ' + path + file)
