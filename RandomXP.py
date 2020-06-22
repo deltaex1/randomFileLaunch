860a8fc5540f29d650b296cb9c524e15c7440334
Wil <35899839+deltaex1@users.noreply.github.com>
2020-06-22 01:26:51 -0400

    Update RandomXP.py
    ensure non-repeat of already selected file, fixed typo on error message

--- RandomXP.py
+++ RandomXP.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 18 20:15:54 2020
-
-@author: Wilson
-"""
 
 import os, secrets
 
-path = ".//"
-#extension = ('.mp4','.wmv','.mkv','.avi','.flv')
-extension = ('jpg','jpeg','png')
+path = ".\\"
+# extension = ('.mp4','.wmv','.mkv','.avi','.flv')
+extension = ('.jpg', '.jpeg', '.png')
+data = []
 
 count = 0
 while count < 5:
     file = (secrets.choice(os.listdir(path)))
     while file.lower().endswith(extension) is False:
         file = (secrets.choice(os.listdir(path)))
-        print('File selected has an incorect extension, retrying...')   
-
-    print('Opening file: ' + path + file)
-    os.startfile(path + file)
-    count+=1
+        print('File selected has an incorrect extension, retrying...')   
+    
+    if file not in data:
+        print('Opening file: ' + path + file)
+        os.startfile(path + file)
+        data.append(file)
+        count+=1
+    else:
+        print('File has already been opened')
+        pass
